cde85a52676dc6c1fd4e79cb2b1a6d98df4854e7
mahios2015 <mahi4care@gmail.com>
2017-01-06 14:36:09 +0530

    Update UnitTest1.cs

--- HelloWorldNUnitTest/UnitTest1.cs
+++ HelloWorldNUnitTest/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace HelloWorldNUnitTest
             Assert.IsTrue(Program.IsDataContainsKeyword("Hello Unit Tester", "Kello"));
         }
          [Test]
-        public void TestMethod1()
+        public void TestMethod2()
         {
             Assert.IsTrue(Program.IsDataContainsKeyword("Hello Unit Tester", "Hello"));
         }
